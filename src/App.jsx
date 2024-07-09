d9e9e0fe915081f7bdad086cf37e70e636911176
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-07-09 15:24:53 +0600

    create folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
       <div>
         <h1 className="text-gray-900 bg-purple-500 text-center text-2xl font-semibold">First REDUX App</h1>
       </div>
+      <Home></Home>
     </>
   );
 }
