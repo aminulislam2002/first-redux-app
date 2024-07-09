c6b7c69223364b9444da41a86daef406cd4e6ef6
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-07-09 16:45:18 +0600

    create counter component usint redux toolkit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,42 @@
-
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByValue, decrementByValue } from "../redux/features/Counter/counterSlice";
 
 const Counter = () => {
-    return (
-        <div>
-            <h1>Counter</h1>
-        </div>
-    );
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="py-10 border">
+      <h1 className="text-center text-lg font-medium text-purple-500 mb-5">Counter Component</h1>
+      <div className="flex justify-center items-center gap-5">
+        <button
+          onClick={() => dispatch(increment())}
+          className="bg-blue-500 hover:bg-blue-600 px-5 py-2 rounded-md text-white text-lg"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => dispatch(incrementByValue(5))}
+          className="bg-blue-500 hover:bg-blue-600 px-5 py-2 rounded-md text-white text-lg"
+        >
+          Increment By Value 5
+        </button>
+        <h1>{count}</h1>
+        <button
+          onClick={() => dispatch(decrementByValue(5))}
+          className="bg-red-500 hover:bg-red-600 px-5 py-2 rounded-md text-white text-lg"
+        >
+          Decrement By Value 5
+        </button>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="bg-red-500 hover:bg-red-600 px-5 py-2 rounded-md text-white text-lg"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-
+import Counter from "../components/Counter";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div className="w-full h-auto max-w-[1240px] mx-auto px-5">
+      <h1>Home</h1>
+      <Counter></Counter>
+    </div>
+  );
 };
 
 export default Home;
